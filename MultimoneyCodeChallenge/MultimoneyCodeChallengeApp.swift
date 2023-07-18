ab2235fe45ace2ba516a33fd37a2ca9162d8fab9
Eduardo Hernandez <lalohernandez93@gmail.com>
2023-07-18 07:15:06 -0600

    Initial Commit

---
+++ MultimoneyCodeChallenge/MultimoneyCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultimoneyCodeChallengeApp.swift
+//  MultimoneyCodeChallenge
+//
+//  Created by Eduardo Hernandez on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MultimoneyCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
